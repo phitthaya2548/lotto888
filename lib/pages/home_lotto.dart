012d11b5eb42a67ed4c7110c28358fa084f1760d
eN-Phutthada <66011212119@msu.ac.th>
2025-09-03 20:21:59 +0700

    Change

--- lib/pages/home_lotto.dart
+++ lib/pages/home_lotto.dart
@@ -7,11 +7,7 @@ class LottoHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-<<<<<<< HEAD
       body: Center(child: Text('Home page')),
-=======
-      body: Center(child: Text('หน้าหลักก จริงงๆ นะ')),
->>>>>>> d5079dd4fd9070b134ffb547ddcf6e718a78925a
     );
   }
 }

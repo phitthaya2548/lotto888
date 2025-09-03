639eea746c8622d6dde05e24f5fea46ac1bdb4d6
sittipong112054 <66011212054@msu.ac.th>
2025-09-03 20:57:03 +0700

    home 1

--- lib/pages/home_lotto.dart
+++ lib/pages/home_lotto.dart
@@ -7,7 +7,7 @@ class LottoHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('Home page')),
+      body: Center(child: Text('Home page 1')),
     );
   }
 }
